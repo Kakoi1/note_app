18ea6e3ff690ea9b3cbadd5a61581d6e918faf4d
KEANMARK <125711782+KEANMARK@users.noreply.github.com>
2024-09-19 23:28:34 +0800

    Update database.php

--- database.php
+++ database.php
@@ -4,8 +4,9 @@ $hostName = "localhost";
 $dbUser = "root";
 $dbPassword = "";
 $dbName = "login_register";
+$port = 3306; // Default MySQL port
 
-$conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
+$conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName, $port);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
@@ -14,6 +15,6 @@ if (!$conn) {
 // Your code for queries here
 
 // Close the connection when done
-
+mysqli_close($conn);
 
 ?>
