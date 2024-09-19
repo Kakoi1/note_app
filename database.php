49556f6c7f59a74c74e814eab1ed93fb09f6130f
KEANMARK <125711782+KEANMARK@users.noreply.github.com>
2024-09-19 22:11:36 +0800

    Update database.php

--- database.php
+++ database.php
@@ -4,9 +4,16 @@ $hostName = "localhost";
 $dbUser = "root";
 $dbPassword = "";
 $dbName = "login_register";
+
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
+
 if (!$conn) {
-    die("connection failed;");
+    die("Connection failed: " . mysqli_connect_error());
 }
+
+// Your code for queries here
+
+// Close the connection when done
+mysqli_close($conn);
 
 ?>
