356a61033aeb3a88fd4db6a5af39acf6b063915c
KEANMARK <125711782+KEANMARK@users.noreply.github.com>
2024-09-23 12:44:47 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,15 +1,11 @@
-
-
 <?php
-session_start(); // Start the session
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
-// Unset all of the session variables
-$_SESSION = array();
-
-// Destroy the session
-session_destroy();
-
-// Redirect to the login page after logging out
-header("Location: login.php");
-exit();
+// Destroy session if active
+if (session_status() == PHP_SESSION_ACTIVE) {
+    session_destroy();
+}
+header('location: index.php');
 ?>
